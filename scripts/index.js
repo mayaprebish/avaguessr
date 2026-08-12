0240ed3673cd704a1e7e60a94ef12d59bbbff34d
Maya Prebish <prebishm@gmail.com>
2026-08-12 15:37:34 -0400

    first

---
+++ scripts/index.js
@@ -0,0 +1,130 @@
+const ava = ["a1.jpg", "a2.jpg", "a3.jpg"];
+const notAva = ["c3.jpg", "c4.jpg", "c5.jpg"];
+var currentAvaPhoto;
+var currentNotAvaPhoto;
+var correctOption;
+var usedPhotos = [];
+var points = 0;
+var turns = 0;
+
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const playAgainButtonElement = document.getElementById("playAgain");
+const img1Element = document.getElementById("option1img");
+const img2Element = document.getElementById("option2img");
+const resultsElement = document.getElementById("result");
+const optionsElement = document.getElementById("options");
+const doneElement = document.getElementById("results");
+option1.addEventListener('click', () => {
+    onClick(1);
+})
+option2.addEventListener('click', () => {
+    onClick(2);
+})
+playAgainButtonElement.addEventListener('click', () => {
+    playAgain();
+})
+
+
+
+// MAIN
+setCorrectOption();
+getNextPhotos();
+img1Element.src = `images/${currentAvaPhoto}`;
+img2Element.src = `images/${currentNotAvaPhoto}`;
+
+function onClick(option) {
+    console.log(option, correctOption);
+    if (option == correctOption) {
+        console.log("correct!");
+        points += 1;
+    } else {
+        console.log("wrong!");
+    }
+
+    toggleFade();
+
+    turns += 1;
+    if (turns < 3) {
+        sleep(500).then(() => {
+        setCorrectOption();
+        getNextPhotos();
+
+        if (correctOption == 1) {
+            img1Element.src = `images/${currentAvaPhoto}`;
+            img2Element.src = `images/${currentNotAvaPhoto}`;
+        } else {
+            img1Element.src = `images/${currentNotAvaPhoto}`;
+            img2Element.src = `images/${currentAvaPhoto}`;
+        }
+
+        toggleFade();
+    });
+    }
+    else {
+        gameOver();
+        console.log("done!");
+        console.log(`points: ${points}/${turns}`);
+    }
+
+
+}
+
+function toggleFade() {
+    option1.classList.toggle('fade-out');
+    option2.classList.toggle('fade-out');
+}
+
+function gameOver() {
+    resultsElement.innerHTML = `${points}/${turns}`;
+    optionsElement.classList.toggle("invisible");
+    doneElement.classList.toggle("invisible");
+}
+
+function playAgain() {
+    points = 0;
+    turns = 0;
+    setCorrectOption();
+    getNextPhotos();
+    optionsElement.classList.toggle("invisible");
+    doneElement.classList.toggle("invisible");
+}
+
+function getNextPhotos() {
+    if (currentAvaPhoto) {
+        usedPhotos.push(currentAvaPhoto);
+    }
+    const remainingAvaPhotos = ava.filter(x => !usedPhotos.includes(x));
+    const remainingNotAvaPhotos = notAva.filter(x => !usedPhotos.includes(x));
+
+    const nextAvaPhoto = remainingAvaPhotos[Math.floor(Math.random()*remainingAvaPhotos.length)];
+    const nextNotAvaPhoto = remainingNotAvaPhotos[Math.floor(Math.random()*remainingNotAvaPhotos.length)];
+
+    currentAvaPhoto = nextAvaPhoto;
+    currentNotAvaPhoto = nextNotAvaPhoto;
+}
+
+function setCorrectOption() {
+    correctOption = Math.floor(Math.random()*2) + 1;
+}
+
+function getNextAvaPhoto() {
+    const remainingPhotos = ava.filter(x => !usedPhotos.includes(x))
+    console.log(remainingPhotos);
+    const nextPhoto = remainingPhotos[Math.floor(Math.random()*remainingPhotos.length)]
+    if (currentAvaPhoto) {
+        usedPhotos.push(currentAvaPhoto);
+    }
+    currentAvaPhoto = nextPhoto;
+}
+
+function getNextNotAvaPhoto() {
+    const remainingPhotos = notAva.filter(x => !usedPhotos.includes(x))
+    const nextPhoto = remainingPhotos[Math.floor(Math.random()*remainingPhotos.length)]
+    if (currentNotAvaPhoto) {
+        usedPhotos.push(currentNotAvaPhoto)
+    }
+    currentNotAvaPhoto = nextPhoto;
+}
